db5738c3584373921ddca6c682c50831c40efa22
Pedro Portugal <ph.portugal03@gmail.com>
2026-03-19 09:15:09 -0300

    Implementação do sistema de bilhete único com cadastro, recarga e catraca

--- 05-exercicios-main/src/exercicio03/Main.java
+++ 05-exercicios-main/src/exercicio03/Main.java
@@ -1,5 +1,6 @@
 package exercicio03;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
@@ -7,10 +8,8 @@ public class Main {
     static Scanner sc = new Scanner(System.in);
     static int index = 0;
 
-
     public static void main(String[] args) {
         int opcao;
-
 
         do {
             System.out.println("\n--- MENU ---");
@@ -23,45 +22,77 @@ public class Main {
 
             switch (opcao) {
                 case 1 -> cadastrar();
-
-
+                case 2 -> recarregar();
+                case 3 -> consultarSaldo();
+                case 4 -> passarNaCatraca();
+                case 5 -> System.out.println("Até breve!");
+                default -> System.out.println("Opção inválida!");
             }
 
-
         } while (opcao != 5);
-
-
     }
 
     private static void cadastrar() {
-        String nome;
-        String tipoTarifa;
-        long cpf;
-
         if (index < bilhete.length) {
             System.out.println("Nome do usuário:");
-            nome = sc.next();
-            System.out.println("CPF: ");
-            cpf = sc.nextLong();
-            ;
-            System.out.println("Tipo (comum / estudante / professor): ");
-            tipoTarifa = sc.next();
+            String nome = sc.next();
+
+            System.out.println("CPF:");
+            long cpf = sc.nextLong();
+
+            System.out.println("Tipo (comum / estudante / professor):");
+            String tipoTarifa = sc.next();
+
             bilhete[index] = new BilheteUnico(new Usuario(cpf, nome, tipoTarifa));
             index++;
+
+            System.out.println("Bilhete cadastrado com sucesso!");
         } else {
-            System.out.println("Erro ao gerar o bilhete! Procure um posto de atendimento ");
+            System.out.println("Limite de bilhetes atingido!");
         }
     }
 
+    private static void recarregar() {
+        BilheteUnico bilheteUnico = pesquisar();
+
+        if (bilheteUnico != null) {
+            System.out.println("Valor da recarga:");
+            double valor = sc.nextDouble();
+
+            bilheteUnico.carregar(valor);
+
+            System.out.println("Novo saldo: " + bilheteUnico.saldo);
+        }
+    }
+
+    private static void consultarSaldo() {
+        DecimalFormat df = new DecimalFormat("#,##0.00");
+        BilheteUnico bilheteUnico = pesquisar();
+        if (bilheteUnico != null) {
+            System.out.println("Saldo atual R$ " +df.format(bilheteUnico.saldo) );
+        }
+    }
+
+    private static void passarNaCatraca() {
+        BilheteUnico bilheteUnico = pesquisar();
+
+        if (bilheteUnico != null) {
+            if (bilheteUnico.passarNaCatraca()) {
+                System.out.println("Passagem liberada!");
+                System.out.println("Saldo restante: " + bilheteUnico.saldo);
+            } else {
+                System.out.println("Saldo insuficiente!");
+            }
+        }
+    }
 
     public static BilheteUnico pesquisar() {
-        long cpf;
-
         System.out.println("Qual o CPF para pesquisa?");
-        cpf = sc.nextLong();
+        long cpf = sc.nextLong();
 
         for (int i = 0; i < index; i++) {
-            if (bilhete[i].usuario.cpf == cpf) {
+            if (bilhete[i] != null &&
+                    bilhete[i].usuario.cpf == cpf) {
                 return bilhete[i];
             }
         }
